8e0cdc6bc8f7d6426790f74a69ddad83b36e62bf
Jakob Dörsing <jakob.doersing@htwg-konstanz.de>
2017-01-18 02:09:33 +0100

    Die wahrscheinlich hässlichste Gui, die die Menschheit je gesehen hat und jemals sehen wird.

--- src/main/scala/de/htwg/se/durak/aview/gui/SwingGui.scala
+++ src/main/scala/de/htwg/se/durak/aview/gui/SwingGui.scala
@@ -13,6 +13,7 @@ import de.htwg.se.util.Observer
 
 class SwingGui(var controller: GameRoundController) extends Frame with Observer {
   final val myFont = new Font("Arial", 0, 25)
+  var numberOfAttack: Int = -1
 
   controller.add(this)
   controller.addSubscriberToRound(this)
@@ -32,16 +33,16 @@ class SwingGui(var controller: GameRoundController) extends Frame with Observer 
   minimumSize = new java.awt.Dimension(framewidth, frameheight)
 
   def getContent = new BoxPanel(Orientation.Vertical) {
-    contents += statusPanel
+    contents += statePanel
     contents += tablePanel
     contents += playerPanel
 
   }
 
-  def statusPanel = new FlowPanel {
+  def statePanel = new FlowPanel {
 
     contents += new Label {
-      text = "Status:"
+      text = "Game state:"
       horizontalAlignment = Alignment.Left
       font = myFont
     }
@@ -63,7 +64,7 @@ class SwingGui(var controller: GameRoundController) extends Frame with Observer 
     }
 
     contents += new Label {
-      text = "Status:"
+      text = "Player state:"
       horizontalAlignment = Alignment.Left
       font = myFont
     }
@@ -78,30 +79,64 @@ class SwingGui(var controller: GameRoundController) extends Frame with Observer 
       horizontalAlignment = Alignment.Left
       font = myFont
     }
-    contents += new TextField {
-      text = controller.getNumberOfCardsInDeck
-      columns = 15
-      font = myFont
-    }
   }
 
   def tablePanel = new GridPanel(1, 2) {
 
+    contents += new Label {
+      font = myFont
+      text = "Number of cards in deck: " + controller.getNumberOfCardsInDeck
+      horizontalAlignment = Alignment.Left
+    }
+
+    contents += new FlowPanel() {
+      for (attack <- controller.getAttacksOnTable) {
+        contents += new BoxPanel(Orientation.Vertical) {
+          contents += new Label {
+            icon = new ImageIcon("D:/Diverses/durak/karten/" + attack.attackingCard.suit + attack.attackingCard.rank + ".jpg")
+            listenTo(mouse.clicks)
+            reactions += {
+              case _: event.MouseClicked => {
+                numberOfAttack = controller.getAttacksOnTable.indexOf(attack)
+                println(numberOfAttack)
+              }
+            }
+
+          }
+          contents += new Label {
+            if (attack.defendingCard != null) {
+              icon = new ImageIcon("D:/Diverses/durak/karten/" + attack.defendingCard.suit + attack.defendingCard.rank + ".jpg")
+            }
+          }
+
+        }
+      }
+
+    }
+
   }
 
   def playerPanel = new FlowPanel() {
-
+    contents += new Label {
+      font = myFont
+      text = "Cards of " + controller.getCurrentPlayerName
+      horizontalAlignment = Alignment.Left
+    }
     for (card <- controller.getCurrentPlayersCards) {
-      /*contents += new Label {
-        font = myFont
-        text = card.suit + " " + card.rank
-        horizontalAlignment = Alignment.Left
-        icon = new ImageIcon("D:/Diverses/durak/karten/a1_big.jpg")
-      }*/
-
       contents += new BoxPanel(Orientation.Vertical) {
         contents += new Label {
-          icon = new ImageIcon("D:/Diverses/durak/karten/a1_big.jpg")
+          icon = new ImageIcon("D:/Diverses/durak/karten/" + card.suit + card.rank + ".jpg")
+          listenTo(mouse.clicks)
+          reactions += {
+            case _: event.MouseClicked => {
+              if (controller.getCurrentPlayerStatus == "Attacker") {
+                controller.playCard(card.suit.toString(), card.rank.toString())
+              } else {
+                controller.playCard(card.suit.toString(), card.rank.toString(), numberOfAttack.toString())
+              }
+            }
+          }
+
         }
         contents += new Label {
           font = myFont
@@ -109,12 +144,25 @@ class SwingGui(var controller: GameRoundController) extends Frame with Observer 
           horizontalAlignment = Alignment.Left
         }
       }
-
+    }
+    contents += new Button("") {
+      action = Action("Finish turn") {
+        controller.endTurn
+        numberOfAttack = -1
+      }
+      font = myFont
+    }
+    contents += new Button() {
+      action = Action("Next round") {
+        controller.updateGameRound
+        numberOfAttack = -1
+      }
+      font = myFont
     }
   }
 
   def redraw = {
-    contents = statusPanel
+    contents = getContent
     repaint
   }
 
